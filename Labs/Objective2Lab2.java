a82cddb3412ddec038dcad47799cce11254e81ad
Osvaldo Maldonado <osvaldomaldonado404@gmail.com>
2023-09-11 15:56:29 -0400

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+	public static void main (String [] args) {
+		String name = "Osvaldo Maldonado";
+		int age = 27;
+		String state = "Georgia";
+		System.out.println(" Name: " + name + " Age: "+ age +" State: "+ state );
+		
+
+	}
+}
