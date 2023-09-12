7d205f78cb18f02f67628bee61c9795a037f82e1
Osvaldo Maldonado <osvaldomaldonado404@gmail.com>
2023-09-12 15:12:51 -0400

    complete Objective3Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,12 @@
+public class Objective3Lab2 {
+	public static void main (String[] args){
+		double side1 = 7;
+		double side2 = 8;
+		double hypotenuse = Math.sqrt(Math.pow(side1,2) + Math.pow(side2,2));
+		System.out.println("The hypotenuse of a triangle with sides 7.0 and 8.0 is ");
+		System.out.println(hypotenuse);
+		
+
+
+	}
+}	
