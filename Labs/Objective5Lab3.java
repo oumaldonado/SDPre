1b5e932db9f05dff1e68cd1dd14555311b9a1cd6
Osvaldo Maldonado <osvaldomaldonado404@gmail.com>
2023-09-14 15:08:11 -0400

    complete Objective5Lab3

---
+++ Labs/Objective5Lab3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Objective5Lab3 {
+	public static void main (String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Please enter a number: ");
+		int userNum = scanner.nextInt();
+
+		if (userNum < 0) {
+			System.out.println("The number is negative");
+
+		} else if (userNum == 0 ) {
+			System.out.println("The number is zero ");
+
+		} else if (userNum > 0) {
+			System.out.println("The number is positive");
+
+		}
+		scanner.close();
+
+
+
+	}
+
+}
