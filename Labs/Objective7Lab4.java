e5f08c8e79f10ee5e10f658f8ac3cf7f3023b53d
Osvaldo Maldonado <osvaldomaldonado404@gmail.com>
2023-09-18 14:22:00 -0400

    complete Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,20 @@
+public class Objective7Lab4 {
+	public static void main (String [] args) {
+		int count = 0;
+		int sum = 0;
+
+
+
+		while(count <= 20) {
+			sum = sum + count;
+			count++;
+
+			if( sum == 210) { 
+			System.out.println(sum);
+		}
+			
+
+
+		}
+	}
+}
