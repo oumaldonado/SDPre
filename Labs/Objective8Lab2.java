cfada73d22a7c8b4abfef6e35b63876cb0f4bd80
Osvaldo Maldonado <osvaldomaldonado404@gmail.com>
2023-09-18 17:46:11 -0400

    complete ObjectiveLlab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,14 @@
+public class Objective8Lab2 {
+	public static void main (String[] args) {
+		int i = 0;
+		int sum = 0;
+
+
+		for (i = 1; i <= 20;i++){
+		 sum = sum + i;
+		 if (sum ==210 ) {
+			System.out.println(sum);
+		}
+		}
+	}
+}
